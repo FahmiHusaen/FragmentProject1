d0557c1197e47635d74f0c3f04de507bbc4c2ce1
@FahmiHusaen18 <muhfahmihusaen@gmail.com>
2022-02-12 21:29:08 +0700

    Finish Task 1. Include a Fragment for the Entire Activity Lifecycle

--- app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.fragmentexample1updated;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
